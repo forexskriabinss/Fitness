c0fcf116d18c0c24a9a0b586f970f754107ad329
Скрябин Сергей <skriabinss@gmail.com>
2020-06-03 16:25:39 +0300

    m

--- Fitness.ConsoleUI/Program.cs
+++ Fitness.ConsoleUI/Program.cs
@@ -28,50 +28,9 @@ namespace Fitness.ConsoleUI
             C.Clear();
             C.WriteLine(userController.CurrentUser);
 
-            while (true)
-            {
-                var choice = UserChoice();
-                C.Clear();
-                switch (choice)
-                {
-                    case ConsoleKey.E:
-                        EatingController eatingController = new EatingController(userController.CurrentUser);
-                        var eating = EnterEating();
-                        eatingController.Add(eating.food, eating.weight);
-                        break;
-                    case ConsoleKey.A:
-                        ExerciseController exerciseController = new ExerciseController(userController.CurrentUser);
-                        var exe = EnterExercise();
-                        exerciseController.Add(exe.activity, exe.start, exe.end);
-                        break;
-                    case ConsoleKey.Q:
-                        Environment.Exit(0);
-                        break;
-                }
 
-            }
-        }
-
-        private static (Activity activity, DateTime start, DateTime end) EnterExercise()
-        {
-            var name = ConsoleHelper.ParseString("Exercise:", clear: false);
-            var calories = ConsoleHelper.ParseDouble("Calories per Minute", clear: false);
-            var start = ConsoleHelper.ParseDateTime("Start time");
-            var end = ConsoleHelper.ParseDateTime("End time");
-            var activity = new Activity(name, calories);
-            return (activity, start, end);
-        }
-
-        private static (Food food, double weight) EnterEating()
-        {
-            var foodName = ConsoleHelper.ParseString("Product name", clear: false);
-            var proteins = ConsoleHelper.ParseDouble("proteins", clear: false);
-            var fats = ConsoleHelper.ParseDouble("fats", clear: false);
-            var carbohydrate = ConsoleHelper.ParseDouble("carbohydrate", clear: false);
-
-            Food food = new Food(foodName, proteins, fats, carbohydrate);
-            var weight = ConsoleHelper.ParseDouble("weight", clear: false);
-            return (food, weight);
+            C.WriteLine(userController.CurrentUser);
+            C.ReadKey();
         }
 
         private static void EnterNewUser(UserController userController)
